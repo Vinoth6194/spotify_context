8c25ea4b99a0c73ca9b9116bd21fb13f3313603c
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-04-13 20:35:14 +0530

    loggd the access token from spotify uri

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { useEffect } from 'react';
 import './App.css';
 import Login from './Login';
+import { getTokenFromUrl } from './spotify';
 
 function App() {
+  //* as the Login comp is rendered from App comp, useEfffecct is used here to execute once
+  useEffect(() => {
+    const token = getTokenFromUrl();
+    console.log('Your token is', token);
+  }, []);
   return (
     <div className="app">
       <Login />
