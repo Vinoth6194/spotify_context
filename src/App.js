8328df1798f79e909dcddaca35db1d3ef4a97dcc
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-04-13 20:45:31 +0530

    save the token the tiken state and logged in the console

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Login from './Login';
 import { getTokenFromUrl } from './spotify';
 
 function App() {
+  const [token, setToken] = useState(null);
   //* as the Login comp is rendered from App comp, useEfffecct is used here to execute once
   useEffect(() => {
     const hash = getTokenFromUrl();
-    const token = hash.access_token;
-    console.log('Your token is', token);
+    const _token = hash.access_token;
+    if (_token) {
+      setToken(_token);
+    }
+
+    console.log('Your token is', _token);
+    // console.log('Token State is', token);
     //* TO hide the acces token from url for security
     window.location.hash = '';
   }, []);
   return (
     <div className="app">
+      {console.log('Token State is', token)}
       <Login />
     </div>
   );
