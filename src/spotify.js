39690b8cbac697d7b2030c4281faf94ca0ad2f38
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-04-13 19:21:20 +0530

    spotify scopes set

--- src/spotify.js
+++ src/spotify.js
@@ -4,3 +4,11 @@
 export const authEndpoint = 'https://accounts.spotify.com/authorize';
 const redirectUrl = 'http://localhost:3000';
 const clientId = '4bf406bb4ba94a3d814510d6d3afd09c';
+
+const scopes = [
+  'user-read-currently-playing',
+  'user-read-recently-played',
+  'user-read-playback-state',
+  'user-top-read',
+  'user-modify-playback-state',
+];
