7890cb00de40779174f5d853adc0abe33444f900
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-04-13 19:09:16 +0530

    authendpoint, redirectUrl, clientid set

--- src/spotify.js
+++ src/spotify.js
@@ -1,2 +1,6 @@
 //* Documentaion Link
 // https://developer.spotify.com/documentation/web-playback-sdk/quick-start/
+
+export const authEndpoint = 'https://accounts.spotify.com/authorize';
+const reddirectUrl = 'http://localhost:3000';
+const clientId = '4bf406bb4ba94a3d814510d6d3afd09c';
