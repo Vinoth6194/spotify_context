e4b79092f6cdeb398daabfaf934c373d0efb5199
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-04-13 19:09:38 +0530

    spelling fix

--- src/spotify.js
+++ src/spotify.js
@@ -2,5 +2,5 @@
 // https://developer.spotify.com/documentation/web-playback-sdk/quick-start/
 
 export const authEndpoint = 'https://accounts.spotify.com/authorize';
-const reddirectUrl = 'http://localhost:3000';
+const redirectUrl = 'http://localhost:3000';
 const clientId = '4bf406bb4ba94a3d814510d6d3afd09c';
