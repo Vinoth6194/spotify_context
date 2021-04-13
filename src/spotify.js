4a18f1efd88f0d0ab580b37d3b6a4e8e7f11edb6
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-04-13 19:29:40 +0530

    access token from resopnse from login url in Login page

--- src/spotify.js
+++ src/spotify.js
@@ -2,7 +2,7 @@
 // https://developer.spotify.com/documentation/web-playback-sdk/quick-start/
 
 export const authEndpoint = 'https://accounts.spotify.com/authorize';
-const redirectUrl = 'http://localhost:3000';
+const redirectUrl = 'http://localhost:3000/';
 const clientId = '4bf406bb4ba94a3d814510d6d3afd09c';
 
 const scopes = [
@@ -12,3 +12,8 @@ const scopes = [
   'user-top-read',
   'user-modify-playback-state',
 ];
+
+//* %20 - ASCII code for space
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUrl}&scope=${scopes.join(
+  '%20'
+)}&response_type=token`;
