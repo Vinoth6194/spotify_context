88bdc63d79800db9d5c4942b01766578e862e516
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-04-13 20:38:07 +0530

    logged the token from the token object from spotify uri

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import { getTokenFromUrl } from './spotify';
 function App() {
   //* as the Login comp is rendered from App comp, useEfffecct is used here to execute once
   useEffect(() => {
-    const token = getTokenFromUrl();
+    const hash = getTokenFromUrl();
+    const token = hash.access_token;
     console.log('Your token is', token);
     //* TO hide the acces token from url for security
     window.location.hash = '';
