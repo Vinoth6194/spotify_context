d9812525e0d1a456e9a28b9bde97c06510c9d3e8
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-04-26 21:33:34 +0530

    condition added  for player comp

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Login from './Login';
 import { getTokenFromUrl } from './spotify';
 import SpotifyWebApi from 'spotify-web-api-js';
+import Player from './Player';
 
 const spotify = new SpotifyWebApi();
 
@@ -30,7 +31,7 @@ function App() {
   return (
     <div className="app">
       {console.log('Token State is', token)}
-      {token ? <h1>Logged in</h1> : <Login />}
+      {token ? <Player /> : <Login />}
     </div>
   );
 }
---
+++ src/Player.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Player() {
+  return <div>Welcome to Spotify</div>;
+}
+
+export default Player;
