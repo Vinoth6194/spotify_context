6baa322e394349aa080974d843b939d264065a2e
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-04-26 21:57:05 +0530

    user dispatched with context api

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import Login from './Login';
 import { getTokenFromUrl } from './spotify';
 import SpotifyWebApi from 'spotify-web-api-js';
 import Player from './Player';
-
+import { useDataLayerValue } from './DataLayer';
 const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken] = useState(null);
+  const [{ user }, dispatch] = useDataLayerValue();
   //* as the Login comp is rendered from App comp, useEfffecct is used here to execute once
   useEffect(() => {
     const hash = getTokenFromUrl();
@@ -21,13 +22,19 @@ function App() {
       spotify.setAccessToken(_token);
       //*return an user obj from the spotify
       spotify.getMe().then(user => {
-        console.log('From Spotify', user);
+        // console.log('From Spotify', user);
+        dispatch({
+          type: 'SET_USER',
+          user: user,
+        });
       });
     }
 
     console.log('Your token is', _token);
     // console.log('Token State is', token);
   }, []);
+  console.log('From Spotify-ContextAPI', user);
+
   return (
     <div className="app">
       {console.log('Token State is', token)}
