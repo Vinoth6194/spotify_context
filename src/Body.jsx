ed30065ae83c5425d6aa7db4ac1ba8e7b01bb4c9
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-04-29 20:25:10 +0530

    playlist description css

--- src/Body.jsx
+++ src/Body.jsx
@@ -5,6 +5,17 @@ function Body({ spotify }) {
   return (
     <div className="body">
       <Header spotify={spotify} />
+      <div className="body__info">
+        <img
+          src="https://www.hypebot.com/wp-content/uploads/2020/07/discover-weekly.png"
+          alt=""
+        />
+        <div className="body__infoText">
+          <strong>PLAYLIST</strong>
+          <h2>Discover Weekly</h2>
+          <p>Description</p>
+        </div>
+      </div>
     </div>
   );
 }
