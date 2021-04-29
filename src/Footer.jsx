8962df67a43b4538c6f03dce85b191080ef55fd7
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-04-29 16:54:37 +0530

    playbar volume controls with matui

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -5,6 +5,9 @@ import SkipNextIcon from '@material-ui/icons/SkipNext';
 import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
 import RepeatIcon from '@material-ui/icons/Repeat';
 import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+import PlaylistPlayIcon from '@material-ui/icons/PlaylistPlay';
+import VolumeDownIcon from '@material-ui/icons/VolumeDown';
+import { Grid, Slider } from '@material-ui/core';
 function Footer() {
   return (
     <div className="footer">
@@ -16,7 +19,19 @@ function Footer() {
         <SkipNextIcon className="footer__icon" />
         <RepeatIcon className="footer__icon" />
       </div>
-      <div className="footer__right">Volue controls</div>
+      <div className="footer__right">
+        <Grid container spacing={2}>
+          <Grid item>
+            <PlaylistPlayIcon />
+          </Grid>
+          <Grid item>
+            <VolumeDownIcon />
+          </Grid>
+          <Grid item xs>
+            <Slider aria-labelledby="continuous-slider" />
+          </Grid>
+        </Grid>
+      </div>
     </div>
   );
 }
