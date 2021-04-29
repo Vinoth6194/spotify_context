aeab9392a6278850bf14a4e4ec5775cf2aec4347
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-04-29 21:35:31 +0530

    songs draft

--- src/Body.jsx
+++ src/Body.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import './Body.css';
 import { useDataLayerValue } from './DataLayer';
 import Header from './Header';
+import PlayCircleFilledIcon from '@material-ui/icons/PlayCircleFilled';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 function Body({ spotify }) {
   const [{ discover_weekly }, dispatch] = useDataLayerValue();
   console.log('Discover_Weekly');
@@ -19,6 +22,13 @@ function Body({ spotify }) {
           <strong>PLAYLIST</strong>
           <h2>Discover Weekly</h2>
           <p>{discover_weekly?.description}</p>
+        </div>
+      </div>
+      <div className="body__songs">
+        <div className="body__icons">
+          <PlayCircleFilledIcon className="body__shuffle" />
+          <FavoriteIcon fontSize="large" />
+          <MoreHorizIcon />
         </div>
       </div>
     </div>
