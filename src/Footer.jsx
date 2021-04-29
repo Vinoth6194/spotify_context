0bfeb9db846b2d8190c3b21cb11feefc1c1df581
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-04-29 16:41:25 +0530

    player controllers with mat ui icons

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import './Footer.css';
+import ShuffleIcon from '@material-ui/icons/Shuffle';
+import SkipNextIcon from '@material-ui/icons/SkipNext';
+import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+import RepeatIcon from '@material-ui/icons/Repeat';
+import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
 function Footer() {
   return (
     <div className="footer">
       <div className="footer__left">album details</div>
-      <div className="footer__center">Player controls</div>
+      <div className="footer__center">
+        <ShuffleIcon className="footer__green" />
+        <SkipPreviousIcon className="footer__icon" />
+        <PlayCircleOutlineIcon className="footer__icon" fontSize="large" />
+        <SkipNextIcon className="footer__icon" />
+        <RepeatIcon className="footer__icon" />
+      </div>
       <div className="footer__right">Volue controls</div>
     </div>
   );
