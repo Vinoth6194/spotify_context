1135e6a5a775afde73077761a269e6034ee16a06
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-04-29 20:46:27 +0530

    logged the dis_weekly playlist

--- src/Body.jsx
+++ src/Body.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import './Body.css';
+import { useDataLayerValue } from './DataLayer';
 import Header from './Header';
 function Body({ spotify }) {
+  const [{ discover_weekly }, dispatch] = useDataLayerValue();
+  console.log('Discover_Weekly');
+  console.log(discover_weekly);
   return (
     <div className="body">
       <Header spotify={spotify} />
