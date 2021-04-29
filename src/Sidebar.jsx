88681a49fbd4136726ff44f82e176e6c072867a8
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-04-29 13:39:30 +0530

    sidebar wtrh icons

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import './Sidebar.css';
 import SidebarOption from './SidebarOption';
+import HomeIcon from '@material-ui/icons/Home';
+import LibraryMusicIcon from '@material-ui/icons/LibraryMusic';
+import SearchIcon from '@material-ui/icons/Search';
 function Sidebar() {
   return (
     <div className="sidebar">
@@ -9,9 +12,9 @@ function Sidebar() {
         src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
         alt=""
       />
-      <SidebarOption title="Home" />
-      <SidebarOption title="Search" />
-      <SidebarOption title="Your Library" />
+      <SidebarOption title="Home" Icon={HomeIcon} />
+      <SidebarOption title="Search" Icon={SearchIcon} />
+      <SidebarOption title="Your Library" Icon={LibraryMusicIcon} />
     </div>
   );
 }
