4c3b73d911a941063895378f9d9353e56e77d9e2
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-04-29 17:06:15 +0530

    playnar song info

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -11,13 +11,22 @@ import { Grid, Slider } from '@material-ui/core';
 function Footer() {
   return (
     <div className="footer">
-      <div className="footer__left">album details</div>
+      <div className="footer__left">
+        <img
+          className="footer__albumLogo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/WWE_Champion_CM_Punk.jpg/170px-WWE_Champion_CM_Punk.jpg"
+        ></img>
+        <div className="footer___songInfo">
+          <h4>Cult of Personality</h4>
+          <p>CM Punk</p>
+        </div>
+      </div>
       <div className="footer__center">
         <ShuffleIcon className="footer__green" />
         <SkipPreviousIcon className="footer__icon" />
         <PlayCircleOutlineIcon className="footer__icon" fontSize="large" />
         <SkipNextIcon className="footer__icon" />
-        <RepeatIcon className="footer__icon" />
+        <RepeatIcon className="footer__icon footer__green" />
       </div>
       <div className="footer__right">
         <Grid container spacing={2}>
