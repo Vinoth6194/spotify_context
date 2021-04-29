ed739ffab96fe4a1091421289dd8fdf4005d5c60
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-04-29 20:05:58 +0530

    name and avatar set with spotify response

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,17 +2,25 @@ import React from 'react';
 import SearchIcon from '@material-ui/icons/Search';
 import './Header.css';
 import { Avatar } from '@material-ui/core';
+import { useDataLayerValue } from './DataLayer';
 function Header() {
+  const [{ user }, dispatch] = useDataLayerValue();
   return (
     <div className="header">
       <div className="header__left">
         <SearchIcon />
         <input placeholder="Search for Artists, Albums or Songs" type="text" />
       </div>
-
+      {/* //* Name and Avatar Image and Name will be displayed dynamically based on
+      //*response only for Premium members */}
+      {/* Once you upgraded to the Premium Plan avatar can be set with */}
+      {/* user?.images[0].url Name with user?,display_name */}
       <div className="header___right">
-        <Avatar src="" alt="VK" />
-        <h4>Vinothkumar</h4>
+        <Avatar
+          src="https://open.spotify.com/user/1992prrtzstq6ptye64ghu4ko"
+          alt="VK"
+        />
+        <h4>{user?.display_name}</h4>
       </div>
     </div>
   );
