dac8127e6ce31bdfb418857fcdf3beac390b391c
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-04-29 19:47:52 +0530

    headerleft

--- src/Body.jsx
+++ src/Body.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import './Body.css';
-function Body() {
+import Header from './Header';
+function Body({ spotify }) {
   return (
     <div className="body">
-      <h1>Body</h1>
+      <Header spotify={spotify} />
     </div>
   );
 }
---
+++ src/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import SearchIcon from '@material-ui/icons/Search';
+import './Header.css';
+import { Avatar } from '@material-ui/core';
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <SearchIcon />
+        <input placeholder="Search for Artists, Albums or Songs" type="text" />
+      </div>
+
+      <div className="headder___right">
+        <Avatar src="" alt="VK" />
+        <h4>Vinothkumar</h4>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
