ce14dd7fe6d2e16ce10832e97aa584b8a1a73228
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-05-03 12:50:13 +0530

    displayed track in rows

--- src/Body.jsx
+++ src/Body.jsx
@@ -5,6 +5,7 @@ import Header from './Header';
 import PlayCircleFilledIcon from '@material-ui/icons/PlayCircleFilled';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import SongRow from './SongRow';
 function Body({ spotify }) {
   const [{ discover_weekly }, dispatch] = useDataLayerValue();
   console.log('Discover_Weekly');
@@ -32,6 +33,9 @@ function Body({ spotify }) {
         </div>
         {console.log('Tracks of Dis Weekly')}
         {console.log(discover_weekly?.tracks.items)}
+        {discover_weekly?.tracks.items.map(item => (
+          <SongRow track={item.track} id={item.track.id} />
+        ))}
       </div>
     </div>
   );
